5c1bd8713f151bdeaebf2087cc92e47f44788f62
c3sare <marcinm222@gmail.com>
2022-09-19 21:38:40 +0200

    heading change in services

--- src/slides/Services.tsx
+++ src/slides/Services.tsx
@@ -13,42 +13,42 @@ const services = [
   {
     title: "Oprogramowanie",
     text: `Tworzymy dedykowane systemy, które są przyjaźnie dostosowane do indywidualnych potrzeb Klienta. Tworzymy aplikacje umożliwiające niezbędny dostęp do informacji z każdego miejsca i o każdej porze.`,
-    icon: <Code/>,
+    icon: <Code />,
   },
   {
     title: "Doradztwo IT",
     text: `Chętnie podzielimy się z Państwem naszą wiedzą i doświadczeniem, tak aby Państwa firma, jak najlepiej wykorzystała możliwości, jakie daje wdrożenie systemów IT. Wybór odpowiednich rozwiązań z dziedziny IT niejednokrotnie decyduje o sukcesie firmy.`,
-    icon: <Handshake/>, 
+    icon: <Handshake />,
   },
   {
     title: "Hosting",
     text: `Hosting to usługa, która udostępnienia miejsca na Twoją stronę WWW na specjalnie do tego przygotowanym serwerze. Jest ona niezbędna, gdy chcemy, aby nasza strona była widoczna w Internecie cały czas.`,
-    icon: <Server/>,
+    icon: <Server />,
   },
   {
     title: "Strony internetowe",
     text: `Internet to szybki i tani sposób na dotarcie do Klientów z całego świata. Projektujemy i tworzymy strony WWW wraz z hostingiem i opieką informatyczną.`,
-    icon: <Web/>,
+    icon: <Web />,
   },
   {
     title: "Poczta elektroniczna",
     text: `Tworzymy pocztę elektroniczną we własnej domenie. Załóż bezpieczną skrzynkę z nazwą doskonale dopasowaną do Twojego nazwiska lub nazwy firmy.`,
-    icon: <Post/>,
+    icon: <Post />,
   },
   {
     title: "Pozycjonowanie / SEO / Adwords",
     text: `Poprawiamy wszystkie elementy, które mają wpływ na pozycję strony w wyszukiwarkach internetowych (np. Google).`,
-    icon: <Ads/>
+    icon: <Ads />,
   },
   {
     title: "Serwis komputerowy",
     text: `Naprawiamy komputery stacjonarne oraz laptopy. Nie działa Ci ekran? Zostałeś zainfekowany, a może potrzebujesz nowego systemu?`,
-    icon: <Tools/>,
+    icon: <Tools />,
   },
   {
     title: "Kopie zapasowe",
     text: `Backup firmowych danych to inaczej tworzenie kopii zapasowej informacji, aby po ich uszkodzeniu lub utracie bez przeszkód je odtworzyć.`,
-    icon: <Backup/>,
+    icon: <Backup />,
   },
 ];
 
@@ -58,7 +58,7 @@ const Services = () => {
       {services.map((service, index) => (
         <div key={index} className={style.service}>
           {service.icon}
-          <h4>{service.title}</h4>
+          <h3>{service.title}</h3>
           <p>{service.text}</p>
           <button>Więcej</button>
         </div>
